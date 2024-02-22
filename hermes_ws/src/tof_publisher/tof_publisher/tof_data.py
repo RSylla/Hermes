2b5adb7d12a6ac69295960368f154a8caad86d73
aleksmalm1 <aleks.malm@eek.ee>
2024-02-22 15:08:24 +0200

    tof subscriber

---
+++ hermes_ws/src/tof_publisher/tof_publisher/tof_data.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+
+class LaserScanSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('laser_scan_subscriber')
+        self.subscription = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Range min: "%f"' % msg.range_min)
+
+def main(args=None):
+    rclpy.init(args=args)
+    laser_scan_subscriber = LaserScanSubscriber()
+    rclpy.spin(laser_scan_subscriber)
+    laser_scan_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
