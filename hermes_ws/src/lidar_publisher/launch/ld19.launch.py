113a6714d6c5202f2fed233d6ca633854eb5dfef
aleksmalm1 <aleks.malm@eek.ee>
2024-02-28 11:10:16 +0200

    crop from 15 to 30 degrees

--- hermes_ws/src/lidar_publisher/launch/ld19.launch.py
+++ hermes_ws/src/lidar_publisher/launch/ld19.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
         {'port_baudrate': 230400},
         {'laser_scan_dir': True},
         {'enable_angle_crop_func': True},
-        {'angle_crop_min': 15.0},
+        {'angle_crop_min': 30.0},
         {'angle_crop_max': 360.0}
       ]
   )
