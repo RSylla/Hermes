70f1f0694f28f1de5ba3042c92f3280744ee4088
aleksmalm1 <aleks.malm@eek.ee>
2024-03-15 11:27:23 +0200

    upd

--- hermes_ws/src/tof_publisher/tof_publisher/tof_data.py
+++ hermes_ws/src/tof_publisher/tof_publisher/tof_data.py
@@ -50,7 +50,7 @@ class LaserScanSubscriber(Node):
                         print("Sector 3 danger: Value =", range_value)
                         self.do_action_for_sector_3 = True
                     elif sector_num == 3:  # Sector 4
-                        print("Sector 4 danger: Value =", range_value)
+                        #print("Sector 4 danger: Value =", range_value)
                         self.do_action_for_sector_4 = True
                 elif range_value > 1.0 or math.isnan(range_value):
                     self.do_action_for_sector_1 = False
@@ -58,7 +58,7 @@ class LaserScanSubscriber(Node):
                     self.do_action_for_sector_3 = False
                     self.do_action_for_sector_4 = False
                 
-                print(self.do_action_for_sector_1, self.do_action_for_sector_2, self.do_action_for_sector_3, self.do_action_for_sector_4)
+                #print(self.do_action_for_sector_1, self.do_action_for_sector_2, self.do_action_for_sector_3, self.do_action_for_sector_4)
 
         # Publish the actions
         self.publish_actions()
