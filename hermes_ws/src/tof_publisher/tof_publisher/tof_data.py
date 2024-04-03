04a77271a5f97a92511d817a5ef0e95a660f5e78
zenbuns <117738390+zenbuns@users.noreply.github.com>
2024-04-03 11:00:42 +0300

    Update tof_data.py working version import from downloads

--- hermes_ws/src/tof_publisher/tof_publisher/tof_data.py
+++ hermes_ws/src/tof_publisher/tof_publisher/tof_data.py
@@ -1,7 +1,9 @@
 import rclpy
+import math
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
-import math
+from std_msgs.msg import Bool
+
 
 class LaserScanSubscriber(Node):
 
@@ -9,49 +11,69 @@ class LaserScanSubscriber(Node):
         super().__init__('laser_scan_subscriber')
         self.subscription = self.create_subscription(
             LaserScan,
-            '/scan',
+            '/ldlidar_node/scan',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.publisher = self.create_publisher(Bool, '/tof_data', 10)
+        self.stop = False
+        #self.do_action_for_sector_1 = False
+        #self.do_action_for_sector_2 = False
+        #self.do_action_for_sector_3 = False
+        #self.do_action_for_sector_4 = False
 
-    def do_action_for_sector_1(self):
-        pass
-
-    def do_action_for_sector_2(self):
-        pass
-
-    def do_action_for_sector_3(self):
-        pass
-
-    def do_action_for_sector_4(self):
-        pass
+    def publish_actions(self):
+        msg = Bool()
+        msg.data = self.stop #self.do_action_for_sector_1 or self.do_action_for_sector_2 or self.do_action_for_sector_3 or self.do_action_for_sector_4
+        self.publisher.publish(msg)
+        print("Published message!")
 
     def listener_callback(self, msg):
         range_start = 0
         range_end = 227
-        sector_size = (range_end - range_start + 1) // 4 
-
+        sector_size = (range_end - range_start + 1) // 4
+        self.stop = False  # Reset the stop flag for each new message
+    
         for sector_num in range(4):
             sector_start = range_start + sector_num * sector_size
             sector_end = sector_start + sector_size - 1
-
+    
             sector_values = msg.ranges[sector_start:sector_end + 1]
-            for range_value in sector_values:  
-                if 0.0 < range_value < 1.0 and not math.isnan(range_value):
+            for range_value in sector_values:
+                if 0.0 < range_value < 0.5 and not math.isnan(range_value):
+                    self.stop = True  # Set the flag to True if a valid range value is found
+                    #print(range_value, self.stop)
+                    self.publish_actions()  # Publish actions immediately
+                elif range_value > 0.5:
+                    self.stop = False  # Reset the flag if a range_value greater than 1.0 is encountered
+                    #print(self.stop)
+                    self.publish_actions()  # Publish actions immediately
+                    break  # Exit the loop as we don't need to check further in this sector
+                
+            #if self.stop:
+               # break  # Exit the loop if the flag is already set
+            
+                    #if sector_num == 0:  # Sector 1
+                    #    self.sector_1_value = range_value
+                    #    self.do_action_for_sector_1 = True 
+                    #elif sector_num == 1:  # Sector 2
+                    #    self.sector_2_value = range_value
+                    #    self.do_action_for_sector_2 = True  
+                    #elif sector_num == 2:  # Sector 3
+                    #    self.sector_3_value = range_value
+                    #    self.do_action_for_sector_3 = True
+                    #elif sector_num == 3:  # Sector 4
+                    #    self.sector_4_value = range_value
+                    #    self.do_action_for_sector_4 = True
+                    #else:
+                    #    self.do_action_for_sector_1 = False
+                    #    self.do_action_for_sector_2 = False
+                    #    self.do_action_for_sector_3 = False
+                    #    self.do_action_for_sector_4 = False
+        #print(self.stop)#[self.do_action_for_sector_1, self.sector_1_value],[self.do_action_for_sector_2, self.sector_2_value],[self.do_action_for_sector_3, self.sector_3_value], [self.do_action_for_sector_4, self.sector_4_value])     
+                #print(self.do_action_for_sector_1, self.do_action_for_sector_2, self.do_action_for_sector_3, self.do_action_for_sector_4)
 
-                    # Actions based on sector
-                    if sector_num == 0:  # Sector 1
-                        print("Sector 1 danger: Value =", range_value)  
-                        self.do_action_for_sector_1()  
-                    elif sector_num == 1:  # Sector 2
-                        print("Sector 2 danger: Value =", range_value)
-                        self.do_action_for_sector_2()  
-                    elif sector_num == 2:  # Sector 3
-                        print("Sector 3 danger: Value =", range_value)
-                        self.do_action_for_sector_3()
-                    elif sector_num == 3:  # Sector 4
-                        print("Sector 4 danger: Value =", range_value)
-                        self.do_action_for_sector_4() 
+        # Publish the actions
+        #self.publish_actions()
 
 def main(args=None):
     rclpy.init(args=args)
