7ef84f84099b7b111e9e9222f24f14dfbdaac2ba
aleksmalm1 <aleks.malm11@gmail.com>
2024-04-26 12:02:44 +0300

    d

--- hermes_ws/install/hermes_urdf/share/hermes_urdf/launch/display.launch.py
+++ hermes_ws/install/hermes_urdf/share/hermes_urdf/launch/display.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(['xacro ', urdf_file_path])}]
     )
 
     # Conditionally launch the Joint State Publisher GUI or non-GUI
@@ -36,8 +36,7 @@ def generate_launch_description():
     joint_state_publisher = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui')),
-        arguments=[urdf_file_path]
+        condition=UnlessCondition(LaunchConfiguration('gui'))
     )
 
     # RViz Visualization
@@ -49,13 +48,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         gui_arg,
-        DeclareLaunchArgument(name='model', default_value=urdf_file_path,
-                              description='Absolute path to robot URDF file'),
-        DeclareLaunchArgument(name='rvizconfig', default_value=rviz_config_path,
-                              description='Absolute path to RViz configuration file'),
         robot_state_publisher,
         joint_state_publisher_gui,
         joint_state_publisher,
         rviz2
     ])
-
--- hermes_ws/src/gps_publisher/src/gps_publisher_node.py
+++ hermes_ws/src/gps_publisher/src/gps_publisher_node.py
@@ -14,7 +14,7 @@ class Gps_publisher_node(Node):
     def __init__(self):
         super().__init__('Gps_publisher_node')
         self.publisher_ = self.create_publisher(GpsFixed, 'gps_data', 10)
-        timer_period = 0.1  # seconds
+        timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.stream = Serial(port="/dev/ttyACM0", baudrate=9600, timeout=0.1)
         self.corr_host = "213.168.5.170"
@@ -34,7 +34,7 @@ class Gps_publisher_node(Node):
         return gps_data
 
     def timer_callback(self):
-        if time() - self.start_time > 1.5:
+        if time() - self.start_time > 2:
             self.send_diff_corrections(self.corr_host, self.corr_port, self.stream)
             self.time = time()
         
@@ -43,22 +43,23 @@ class Gps_publisher_node(Node):
         gps_data = self.recieve_gps_data(self.stream)
         data = NMEAReader.parse(gps_data)
         if data.msgID == "UBX":   
-             if data.msgId == "00":           
-                msg.diff_age = data.diffAge
-                msg.is_corrected = True if data.diffAge < 5 else False
-                msg.message_id = f"{data.msgID} {data.Id}"
-                msg.utc_time = data.time
-                msg.latitude = data.lat
-                msg.longtitude = data.long
-                msg.north_south = data.NS
-                msg.east_west = data.EW
-                msg.nav_status = data.NavStat
-                msg.hor_accuracy = data.hAcc
-                msg.ver_accuracy = data.vAcc
-                msg.speed_over_ground_kmh = data.SOG
-                msg.course_over_ground_deg = data.COG
-                msg.vertical_vel_ms = data.vVel
-                msg.num_sat = data.numSvs
+             if data.msgId == "00":
+                print(data)
+                msg.diff_age = float(data.diffAge) if not data.diffAge == "" else float(99.99)
+                msg.is_corrected = True if msg.diff_age < 5 else False
+                msg.message_id = f"{data.msgID} {data.msgId}"
+                msg.utc_time = str(data.time)
+                msg.latitude = float(data.lat)
+                msg.longtitude = float(data.lon)
+                msg.north_south = str(data.NS)
+                msg.east_west = str(data.EW)
+                msg.nav_status = str(data.navStat)
+                msg.hor_accuracy = float(data.hAcc)
+                msg.ver_accuracy = float(data.vAcc)
+                msg.speed_over_ground_kmh = float(data.SOG)
+                msg.course_over_ground_deg = float(data.COG)
+                msg.vertical_vel_ms = float(data.vVel)
+                msg.num_sat = int(data.numSVs)
                 
         self.publisher_.publish(msg)
                 
