dd0705f80369f6da232f8effa794a6f9bf4c80c9
Raivo Sülla <raivo.sylla@gmail.com>
2024-10-03 13:07:27 +0300

    Deleted everything

--- hermes_ws/build/teleop_control/build/lib/teleop_control/teleop_control_node.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from teleop_control.')
-
-
-if __name__ == '__main__':
-    main()
--- hermes_ws/build/teleop_control/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/aleksmalm/Hermes/hermes_ws/install/teleop_control'
