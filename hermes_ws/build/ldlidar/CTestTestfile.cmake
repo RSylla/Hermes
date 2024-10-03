07342b34f70e7fcfb692d2d94edb32b860ea04b4
Raivo Sülla <raivo.sylla@gmail.com>
2024-10-03 13:05:51 +0300

    Deleted everything

--- hermes_ws/build/ldlidar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aleksmalm/Hermes/hermes_ws/src/lidar_publisher/ldlidar
-# Build directory: /home/aleksmalm/Hermes/hermes_ws/build/ldlidar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- hermes_ws/build/ldlidar_node/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/aleksmalm/Hermes/hermes_ws/install/ldlidar_component/share/ldlidar_component/package.sh"
