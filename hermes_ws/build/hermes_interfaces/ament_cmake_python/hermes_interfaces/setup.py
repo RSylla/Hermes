76fe8c81823286c8451a51860901d4e7215696e5
Raivo Sülla <raivo.sylla@gmail.com>
2024-10-03 13:06:34 +0300

    Deleted everything

--- hermes_ws/build/hermes_interfaces/ament_cmake_python/hermes_interfaces/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='hermes_interfaces',
-    version='0.0.0',
-    packages=find_packages(
-        include=('hermes_interfaces', 'hermes_interfaces.*')),
-)
--- hermes_ws/build/ldlidar/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/aleksmalm/Hermes/hermes_ws/install/ldlidar_component/share/ldlidar_component/package.sh"
-. "/home/aleksmalm/Hermes/hermes_ws/install/ldlidar_node/share/ldlidar_node/package.sh"
